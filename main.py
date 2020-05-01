b4d909c6a57590241b1e3163d6d905a0600bb763
Marc-Antoine Plourde <56414802+map555@users.noreply.github.com>
2020-05-01 17:20:16 -0400

    Update main.py
    Add damage sensor and it's output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 
+
 import time
 
 import matplotlib
@@ -10,6 +11,7 @@ import seaborn as sns
 
 from beamngpy import BeamNGpy, Vehicle, Scenario
 from beamngpy.sensors import Electrics
+from beamngpy.sensors import Damage
 
 sns.set()  # Make seaborn set matplotlib styling
 
@@ -25,7 +27,10 @@ vehicle = Vehicle('ego', model='etkc', licence='LIFLAB', colour='Blue')
 electrics = Electrics()
 vehicle.attach_sensor('electrics', electrics)
 
+#Create a Damage sensor and attach it to the vehicle
 
+damage=Damage()
+vehicle.attach_sensor('damage',damage)
 
 # Create a scenario called vehicle_state taking place in the gridmap map the simulator ships with
 scenario = Scenario('gridmap', 'vehicle_state')
@@ -33,7 +38,7 @@ scenario = Scenario('gridmap', 'vehicle_state')
 # The position & orientation values were obtained by opening the level in the simulator,
 # hitting F11 to open the editor and look for a spot to spawn and simply noting down the
 # corresponding values.
-scenario.add_vehicle(vehicle, pos=(-717.121, 101, 0), rot=(0, 0, 45))  # 45 degree rotation around the z-axis
+scenario.add_vehicle(vehicle, pos=(0.0, 0, 0), rot=(0, 0, 45))  # 45 degree rotation around the z-axis
 
 # The make function of a scneario is used to compile the scenario and produce a scenario file the simulator can load
 scenario.make(beamng)
@@ -44,7 +49,7 @@ bng.load_scenario(scenario)
 bng.start_scenario()  # After loading, the simulator waits for further input to actually start
 
 
-vehicle.ai_set_mode('span')
+vehicle.ai_set_mode('disabled')
 
 positions = list()
 directions = list()
@@ -103,9 +108,10 @@ print("\tDirection:", vehicle.state['dir'])
 print("\tSteering wheel:", sensors['electrics']['values']['steering'])
 print()
 
+
 print("\nLOOP STARTS NOW\n\n\n\n")
 
-for x in range(3000):
+for x in range(30000):
     time.sleep(0.01)
     vehicle.update_vehicle()  # Synchs the vehicle's "state" variable with the simulator
     sensors = bng.poll_sensors(vehicle)  # Polls the data of all sensors attached to the vehicle
@@ -156,7 +162,114 @@ for x in range(3000):
     print("\t\tSteering wheel:",sensors['electrics']['values']['steering'])
     print()
 
+
+
+    print("\tDAMAGE")
+    print("\t\tLOW PRESSURE:",sensors['damage']['lowpressure'])
+    print("\t\tDAMAGE:",sensors['damage']['damageExt'])
+
+
+
+    print("\t\tDEFORM GROUP DAMAGE:",sensors['damage']['damage'])
+    print("\t\t\tMECANICAL")
+    print("\t\t\t\tRADIATOR")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['radiator_damage']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['radiator_damage']['eventCount'])
+
+    print("\t\t\t\tRADIATOR TUBING")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['radtube_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['radtube_break']['eventCount'])
+
+    print("\t\t\t\tDRIVESHAFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['driveshaft']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['driveshaft']['eventCount'])
+
+
+
+    print("\t\t\tGLASS")
+    print("\t\t\t\tWINDSHIELD")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['windshield_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['windshield_break']['eventCount'])
+
+    print("\t\t\t\tSIDE GLASS LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['sideglass_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['sideglass_L_break']['eventCount'])
+
+    print("\t\t\t\tSIDE GLASS RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['sideglass_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['sideglass_R_break']['eventCount'])
+
+    print("\t\t\t\tDOOR GLASS LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['doorglass_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['doorglass_L_break']['eventCount'])
+
+    print("\t\t\t\tDOOR GLASS RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['doorglass_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['doorglass_R_break']['eventCount'])
+
+    print("\t\t\t\tHEAD LIGHT GLASS LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['headlightglass_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['headlightglass_L_break']['eventCount'])
+
+    print("\t\t\t\tHEAD LIGHT GLASS RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['headlightglass_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['headlightglass_R_break']['eventCount'])
+
+    print("\t\t\t\tFOG LIGHT GLASS LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['foglightglass_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['foglightglass_L_break']['eventCount'])
+
+    print("\t\t\t\tFOG LIGHT GLASS RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['foglightglass_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['foglightglass_R_break']['eventCount'])
+
+    print("\t\t\t\tTAIL LIGHT GLASS LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['taillightglass_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['taillightglass_L_break']['eventCount'])
+
+    print("\t\t\t\tTAIL LIGHT GLASS RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['taillightglass_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['taillightglass_R_break']['eventCount'])
+
+
+
+    print("\t\t\tELECTRIC")
+    print("\t\t\t\tBACKLIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['backlight_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['backlight_break']['eventCount'])
+
+    print("\t\t\t\tMIRROR SIGNAL LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['mirrorsignal_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['mirrorsignal_L_break']['eventCount'])
+
+    print("\t\t\t\tMIRROR SIGNAL RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['mirrorsignal_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['mirrorsignal_R_break']['eventCount'])
+
+    print("\t\t\t\tTAIL LIGHT LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['taillight_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['taillight_L_break']['eventCount'])
+
+    print("\t\t\t\tTAIL LIGHT RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['taillight_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['taillight_R_break']['eventCount'])
+
+    print("\t\t\t\tTRUNK LIGHT LEFT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['trunklight_L_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['trunklight_L_break']['eventCount'])
+
+    print("\t\t\t\tTRUNK LIGHT RIGHT")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['trunklight_R_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['trunklight_R_break']['eventCount'])
+
+    print("\t\t\t\tCENTER HIGH MOUNTED STOP LAMP")
+    print("\t\t\t\t\tDamage:", sensors['damage']['deformGroupDamage']['chmsl_break']['damage'])
+    print("\t\t\t\t\tEvent count:", sensors['damage']['deformGroupDamage']['chmsl_break']['eventCount'])
+
+
+
     print("\n\n")
+
 
 
 
