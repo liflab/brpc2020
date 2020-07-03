7d0ac5b875b491f798df06a725c5b3d6ae658047
Marc-Antoine Plourde <56414802+map555@users.noreply.github.com>
2020-07-03 16:07:27 -0400

    Refactor GForce.java

--- Test/BeepBeep/10Properties/2- GForce/Parking/src/GForce.java
+++ Test/BeepBeep/10Properties/2- GForce/Parking/src/GForce.java
@@ -1,37 +1,50 @@
 import ca.uqac.lif.cep.Connector;
 import ca.uqac.lif.cep.Pullable;
+import ca.uqac.lif.cep.functions.ApplyFunction;
 import ca.uqac.lif.cep.functions.Cumulate;
 import ca.uqac.lif.cep.functions.CumulativeFunction;
+import ca.uqac.lif.cep.functions.FunctionTree;
 import ca.uqac.lif.cep.io.ReadLines;
 import ca.uqac.lif.cep.io.WriteToFile;
 import ca.uqac.lif.cep.json.JPathFunction;
+import ca.uqac.lif.cep.json.NumberValue;
 import ca.uqac.lif.cep.json.ParseJson;
 import ca.uqac.lif.cep.mtnp.DrawPlot;
 import ca.uqac.lif.cep.mtnp.UpdateTable;
 import ca.uqac.lif.cep.mtnp.UpdateTableStream;
+import ca.uqac.lif.cep.tmf.Fork;
+import ca.uqac.lif.cep.tmf.KeepLast;
 import ca.uqac.lif.cep.tmf.Pump;
 import ca.uqac.lif.cep.tmf.QueueSource;
 import ca.uqac.lif.cep.util.Numbers;
 import ca.uqac.lif.json.JsonMap;
-import ca.uqac.lif.mtnp.plot.gral.Scatterplot;
+import ca.uqac.lif.mtnp.plot.gnuplot.Scatterplot;
 
 import java.io.InputStream;
+import java.text.DecimalFormat;
 
 public class GForce {
     public static void main(String[] args) {
         InputStream is= GForce.class.getResourceAsStream("static.txt");
         ReadLines read=new ReadLines(is);
-        Pullable readp=read.getPullableOutput();
 
         //to do: implement fork
-        QueueSource xGForce=new QueueSource().loop(false);
-        QueueSource yGForce=new QueueSource().loop(false);
-        QueueSource zGForce=new QueueSource().loop(false);
-        QueueSource xTablesAxis=new QueueSource();
-        QueueSource yTablesAxis=new QueueSource();
-        QueueSource zTablesAxis=new QueueSource();
+
+        QueueSource tableXAxis=new QueueSource().loop(false);
 
 
+        ApplyFunction parseData=new ApplyFunction(ParseJson.instance);
+        ApplyFunction jpfGForceData=new ApplyFunction(new JPathFunction("data.gforce"));
+
+        Fork gForceFork=new Fork(3);
+        Fork gxFork=new Fork(3);
+        Fork gyFork=new Fork(3);
+        Fork gzFork=new Fork(3);
+        Fork tableXAxisFork=new Fork(3);
+
+        ApplyFunction jpfGX=new ApplyFunction((new FunctionTree(NumberValue.instance, new JPathFunction("gx"))));
+        ApplyFunction jpfGY=new ApplyFunction((new FunctionTree(NumberValue.instance, new JPathFunction("gy"))));
+        ApplyFunction jpfGZ=new ApplyFunction((new FunctionTree(NumberValue.instance, new JPathFunction("gz"))));
 
         Cumulate xMax=new Cumulate(new CumulativeFunction<Number>(Numbers.maximum));
         Cumulate xMin=new Cumulate(new CumulativeFunction<Number>(Numbers.minimum));
@@ -40,20 +53,18 @@ public class GForce {
         Cumulate zMax=new Cumulate(new CumulativeFunction<Number>(Numbers.maximum));
         Cumulate zMin=new Cumulate(new CumulativeFunction<Number>(Numbers.minimum));
 
-        UpdateTable xTable=new UpdateTableStream("time(second)","gforce???");
-        UpdateTable yTable=new UpdateTableStream("time(second)","gforce???");
-        UpdateTable zTable=new UpdateTableStream("time(second)","gforce???");
+        UpdateTable xTable=new UpdateTableStream("time(second)","gforce");
+        UpdateTable yTable=new UpdateTableStream("time(second)","gforce");
+        UpdateTable zTable=new UpdateTableStream("time(second)","gforce");
+
+        KeepLast klX=new KeepLast();
+        KeepLast klY=new KeepLast();
+        KeepLast klZ=new KeepLast();
+
 
         DrawPlot xDraw= new DrawPlot(new Scatterplot());
         DrawPlot yDraw= new DrawPlot(new Scatterplot());
         DrawPlot zDraw= new DrawPlot(new Scatterplot());
-
-        Pullable xMaxPull=xMax.getPullableOutput();
-        Pullable xMinPull=xMin.getPullableOutput();
-        Pullable yMaxPull=yMax.getPullableOutput();
-        Pullable yMinPull=yMin.getPullableOutput();
-        Pullable zMaxPull=zMax.getPullableOutput();
-        Pullable zMinPull=zMin.getPullableOutput();
 
         WriteToFile xWrite =new WriteToFile("Static_xGForce.png");
         WriteToFile yWrite =new WriteToFile("Static_yGForce.png");
@@ -63,69 +74,86 @@ public class GForce {
         Pump yPump=new Pump();
         Pump zPump=new Pump();
 
+        Connector.connect(read,parseData);
+        Connector.connect(parseData,jpfGForceData);
+        Connector.connect(jpfGForceData,gForceFork);
+        Connector.connect(gForceFork,0,jpfGX,0);
+        Connector.connect(gForceFork,1,jpfGY,0);
+        Connector.connect(gForceFork,2,jpfGZ,0);
+        Connector.connect(jpfGX,gxFork);
+        Connector.connect(jpfGY,gyFork);
+        Connector.connect(jpfGZ,gzFork);
+        Connector.connect(gxFork,0,xMin,0);
+        Connector.connect(gxFork,1,xMax,0);
+        Connector.connect(gyFork,0,yMin,0);
+        Connector.connect(gyFork,1,yMax,0);
+        Connector.connect(gzFork,0,zMin,0);
+        Connector.connect(gzFork,1,zMax,0);
 
-        Connector.connect(xGForce,xMax);
-        Connector.connect(xGForce,xMin);
-        Connector.connect(yGForce,yMax);
-        Connector.connect(yGForce,yMin);
-        Connector.connect(zGForce,zMax);
-        Connector.connect(zGForce,zMin);
+        Connector.connect(tableXAxis,tableXAxisFork);
 
-        Connector.connect(xTablesAxis,0,xTable,0);
-        Connector.connect(xGForce,0,xTable,1);
-        Connector.connect(xTable,xDraw);
-        Connector.connect(xTable,xWrite);
+        Connector.connect(tableXAxisFork,0,xTable,0);
+        Connector.connect(gxFork,2,xTable,1);
+        Connector.connect(xTable,klX);
+        Connector.connect(klX,xDraw);
+        Connector.connect(klX,xWrite);
         Connector.connect(xPump,xWrite);
         Connector.connect(xDraw,xPump);
 
-        Connector.connect(yTablesAxis,0,yTable,0);
-        Connector.connect(yGForce,0,yTable,1);
-        Connector.connect(yTable,yDraw);
-        Connector.connect(yTable,yWrite);
+        Connector.connect(tableXAxisFork,1,yTable,0);
+        Connector.connect(gyFork,2,yTable,1);
+        Connector.connect(yTable,klY);
+        Connector.connect(klY,yDraw);
+        Connector.connect(klY,yWrite);
         Connector.connect(yPump,yWrite);
         Connector.connect(yDraw,yPump);
 
-        Connector.connect(zTablesAxis,0,zTable,0);
-        Connector.connect(zGForce,0,zTable,1);
-        Connector.connect(zTable,zDraw);
-        Connector.connect(zTable,zWrite);
+        Connector.connect(tableXAxisFork,2,zTable,0);
+        Connector.connect(gzFork,2,zTable,1);
+        Connector.connect(zTable,klZ);
+        Connector.connect(klZ,zDraw);
+        Connector.connect(klZ,zWrite);
         Connector.connect(zPump,zWrite);
         Connector.connect(zDraw,zPump);
 
+        double time=0;
+        double gxMaxValue=0;
+        double gxMinValue=0;
+        double gyMaxValue=0;
+        double gyMinValue=0;
+        double gzMaxValue=0;
+        double gzMinValue=0;
 
+        Pullable xMaxPull=xMax.getPullableOutput();
+        Pullable xMinPull=xMin.getPullableOutput();
+        Pullable yMaxPull=yMax.getPullableOutput();
+        Pullable yMinPull=yMin.getPullableOutput();
+        Pullable zMaxPull=zMax.getPullableOutput();
+        Pullable zMinPull=zMin.getPullableOutput();
 
+        DecimalFormat decimalFormat=new DecimalFormat("#.000");
 
-        float time=0;
-        Object[] out= new Object[1];
+        while(xMaxPull.hasNext()){
 
+            gxMaxValue=((Number) xMaxPull.pull()).doubleValue();
+            gxMinValue=((Number) xMinPull.pull()).doubleValue();
+            gyMaxValue=((Number) yMaxPull.pull()).doubleValue();
+            gyMinValue=((Number) yMinPull.pull()).doubleValue();
+            gzMaxValue=((Number) zMaxPull.pull()).doubleValue();
+            gzMinValue=((Number) zMinPull.pull()).doubleValue();
 
-
-        while(readp.hasNext()){
-
-            ParseJson.instance.evaluate(new Object[]{String.valueOf(readp.pull())},out);
-            JsonMap dictionnary=(JsonMap) out[0];
-
-            xGForce.addEvent(getData(getSubDict(getSubDict(dictionnary,"data"),"gforce"),"gx"));
-            yGForce.addEvent(getData(getSubDict(getSubDict(dictionnary,"data"),"gforce"),"gy"));
-            zGForce.addEvent(getData(getSubDict(getSubDict(dictionnary,"data"),"gforce"),"gz"));
-
-            xTablesAxis.addEvent(time);
-            yTablesAxis.addEvent(time);
-            zTablesAxis.addEvent(time);
-
+            tableXAxis.addEvent(time);
             time+=0.10;
-
-
-
 
         }
 
-        System.out.println("X GForce max: "+xMaxPull.pull());
-        System.out.println("X GForce min: "+xMinPull.pull());
-        System.out.println("Y GForce max: "+yMaxPull.pull());
-        System.out.println("Y GForce min: "+yMinPull.pull());
-        System.out.println("Z GForce max: "+zMaxPull.pull());
-        System.out.println("Z GForce min: "+zMinPull.pull());
+        System.out.println("X GForce max: "+decimalFormat.format(gxMaxValue));
+        System.out.println("X GForce min: "+decimalFormat.format(gxMinValue));
+        System.out.println("Y GForce max: "+decimalFormat.format(gyMaxValue));
+        System.out.println("Y GForce min: "+decimalFormat.format(gyMinValue));
+        System.out.println("Z GForce max: "+decimalFormat.format(gzMaxValue));
+        System.out.println("Z GForce min: "+decimalFormat.format(gzMinValue));
+
         xPump.run();
         yPump.run();
         zPump.run();
@@ -133,21 +161,5 @@ public class GForce {
 
     }
 
-    public static JsonMap getSubDict(JsonMap dict, String wantedDictName){
-        Object[] out = new Object[1];
-        JPathFunction data=new JPathFunction(wantedDictName);
-        data.evaluate(new Object[]{dict},out);
-        JsonMap subDict=(JsonMap) out[0];
-
-        return subDict;
-
-    }
-
-    public static Object getData(JsonMap dict,String elementName){
-        Object data= dict.getNumber(elementName);
-
-        return data;
-
-    }
 
 }
