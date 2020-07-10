5fb81efe78b1adfd15c1210c3e78c04cfcd44ce0
Marc-Antoine Plourde <56414802+map555@users.noreply.github.com>
2020-07-10 10:49:37 -0400

    add property9

--- Test/BeepBeep/10Properties/9- MinimalDistance/src/MinimalDistance.java
+++ Test/BeepBeep/10Properties/9- MinimalDistance/src/MinimalDistance.java
@@ -1,16 +1,17 @@
 import ca.uqac.lif.cep.Connector;
 import ca.uqac.lif.cep.Pullable;
-import ca.uqac.lif.cep.functions.ApplyFunction;
-import ca.uqac.lif.cep.functions.Constant;
-import ca.uqac.lif.cep.functions.FunctionTree;
+import ca.uqac.lif.cep.functions.*;
 import ca.uqac.lif.cep.io.ReadLines;
 import ca.uqac.lif.cep.json.JPathFunction;
 import ca.uqac.lif.cep.json.NumberValue;
 import ca.uqac.lif.cep.json.ParseJson;
 import ca.uqac.lif.cep.tmf.Fork;
 import ca.uqac.lif.cep.tmf.QueueSource;
+import ca.uqac.lif.cep.util.Numbers;
 
 import java.io.InputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MinimalDistance {
     public static void main(String[] args) {
@@ -18,24 +19,45 @@ public class MinimalDistance {
         InputStream is=MinimalDistance.class.getResourceAsStream("dictionnary.txt");
         ReadLines reader=new ReadLines(is);
 
-        QueueSource segments=new QueueSource().setEvents(new Segment(new Point(1.0, 1.0),new Point(3.0,3.0)),new Segment(3.0,3.0,3.0,5.0));
+        QueueSource segmentsSource=new QueueSource().setEvents(new Segment(new Point(1.0, 1.0),new Point(1.0,3.0)),new Segment(1.0,3.0,1.0,5.0));
+        QueueSource segmentsSourceCopy=segmentsSource;
+        Pullable pullSegments=segmentsSourceCopy.getPullableOutput();
 
-        ApplyFunction parseData=new ApplyFunction(ParseJson.instance);
+       ApplyFunction parseData=new ApplyFunction(ParseJson.instance);
         ApplyFunction jpfPosition=new ApplyFunction(new JPathFunction("data.position and direction.position"));
         Fork positionFork=new Fork(2);
         ApplyFunction jpfX=new ApplyFunction((new FunctionTree(NumberValue.instance, new JPathFunction("x"))));
-        Fork xFork=new Fork(3);
-        ApplyFunction jpfY=new ApplyFunction((new FunctionTree(NumberValue.instance, new JPathFunction("y"))));
-        Fork yFork=new Fork(3);
 
+        ApplyFunction jpfY=new ApplyFunction((new FunctionTree(NumberValue.instance, new JPathFunction("y"))));
+
+        Constant segmentConst=new Constant(pullSegments);
+        FunctionTree evaluateMinDistFT=new FunctionTree(new evaluateDistance(), StreamVariable.X,StreamVariable.Y,segmentConst);
+        ApplyFunction evaluateMinDist=new ApplyFunction(evaluateMinDistFT);
+        Cumulate sum=new Cumulate(new CumulativeFunction<Number>(Numbers.addition));
 
         Connector.connect(reader,parseData);
         Connector.connect(parseData,jpfPosition);
         Connector.connect(jpfPosition,positionFork);
         Connector.connect(positionFork,0,jpfX,0);// x Position
-        Connector.connect(jpfX,xFork);
         Connector.connect(positionFork,1,jpfY,0);// y Position
-        Connector.connect(jpfY,yFork);
+        Connector.connect(jpfX,0,evaluateMinDist,0);
+        Connector.connect(jpfY,0,evaluateMinDist,1);
+        Connector.connect(evaluateMinDist,sum);
+
+        Pullable sumP=sum.getPullableOutput();
+
+        while(sumP.hasNext()){
+
+            System.out.println(sumP.pull());
+            System.out.println();
+            segmentsSourceCopy=segmentsSource;//If it's stupid and it's work, it's not stupid.
+        }
+
+
+
+
+
+
 
 
 
--- Test/BeepBeep/10Properties/9- MinimalDistance/src/Segment.java
+++ Test/BeepBeep/10Properties/9- MinimalDistance/src/Segment.java
@@ -1,20 +1,29 @@
 public class Segment {
     private Point aPoint;
     private Point bPoint;
+    private double xLenght;
+    private double yLenght;
 
 
     public Segment(Point aPoint, Point bPoint) {
         this.aPoint = aPoint;
         this.bPoint = bPoint;
+        setLenght(this.aPoint,this.bPoint);
 
     }
 
     public Segment(double x1,double x2,double y1,double y2) {
         aPoint=new Point(x1,y1);
         bPoint=new Point(x2,y2);
+        setLenght(aPoint,bPoint);
 
 
 
+    }
+
+    private void setLenght(Point a,Point b){
+        xLenght=b.getX()-a.getX();
+        yLenght=b.getY()-a.getY();
     }
 
     public Point getaPoint() {
@@ -26,20 +35,35 @@ public class Segment {
     }
 
     public void setaPoint(Point aPoint) {
+
         this.aPoint = aPoint;
+        setLenght(this.aPoint,bPoint);
     }
 
     public void setaPoint(double x,double y) {
         aPoint.setX(x);
         aPoint.setY(y);
+        setLenght(aPoint,bPoint);
     }
 
     public void setbPoint(Point bPoint) {
         this.bPoint = bPoint;
+        setLenght(aPoint,this.bPoint);
+
     }
 
     public void setbPoint(double x,double y) {
         bPoint.setX(x);
         bPoint.setY(y);
+        setLenght(aPoint,bPoint);
+
+    }
+
+    public double getyLenght() {
+        return yLenght;
+    }
+
+    public double getxLenght() {
+        return xLenght;
     }
 }
