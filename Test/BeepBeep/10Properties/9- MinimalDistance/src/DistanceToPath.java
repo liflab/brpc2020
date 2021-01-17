ec2f650bb90ea171323d8b202d4ec553af172757
marc <marc-antoine.plourde1@uqac.ca>
2021-01-17 15:36:43 -0500

    Update DistanceToPath.java

--- Test/BeepBeep/10Properties/9- MinimalDistance/src/DistanceToPath.java
+++ Test/BeepBeep/10Properties/9- MinimalDistance/src/DistanceToPath.java
@@ -25,87 +25,88 @@ import ca.uqac.lif.cep.functions.UnaryFunction;
  */
 public class DistanceToPath extends UnaryFunction<Point,Number>
 {
-  /**
-   * The segments that make the path.
-   */
-  protected Segment[] m_segments;
-  
-  /**
-   * Creates an instance of of the function.
-   * @param segments The linear segments that define the path. The segments
-   * do not need to be listed in contiguous order. No check is made to ensure
-   * that the segments form a connected path.
-   */
-  public DistanceToPath(Segment ... segments)
-  {
-    super(Point.class, Number.class);
-    m_segments = segments;
-  }
-  
-  @Override
-  public Number getValue(Point p)
-  {
-    if (m_segments.length == 0)
+    /**
+     * The segments that make the path.
+     */
+    protected Segment[] m_segments;
+
+    /**
+     * Creates an instance of of the function.
+     * @param segments The linear segments that define the path. The segments
+     * do not need to be listed in contiguous order. No check is made to ensure
+     * that the segments form a connected path.
+     */
+    public DistanceToPath(Segment ... segments)
     {
-      return 0;
+        super(Point.class, Number.class);
+        m_segments = segments;
     }
-    double min_distance = -1;
-    for (Segment s : m_segments)
+
+    @Override
+    public Number getValue(Point p)
     {
-      double d = distanceToSegment(p, s);
-      if (min_distance < 0 || d < min_distance)
-      {
-        min_distance = d;
-      }
+        if (m_segments.length == 0)
+        {
+            return 0;
+        }
+        double min_distance = -1;
+        for (Segment s : m_segments)
+        {
+            double d = distanceToSegment(p, s);
+            if (min_distance < 0 || d < min_distance)
+            {
+                min_distance = d;
+            }
+        }
+        return min_distance;
     }
-    return min_distance;
-  }
-  
-  /**
-   * Computes the shortest distance between a point and a finite line segment.
-   * This code has been adapted from a post on
-   * <a href="https://stackoverflow.com/a/6853926">StackOverflow</a>.
-   * @param p The point
-   * @param s The segment
-   * @return The shortest distance
-   */
-  protected static double distanceToSegment(Point p, Segment s)
-  {
-    double x = p.getX();
-    double y = p.getY();
-    double x1 = s.getaPoint().getX();
-    double y1 = s.getaPoint().getY();
-    double x2 = s.getbPoint().getX();
-    double y2 = s.getbPoint().getY();
-    double A = x - x1;
-    double B = y - y1;
-    double C = x2 - x1;
-    double D = y2 - y1;
-    double dot = A * C + B * D;
-    double len_sq = C * C + D * D;
-    double param = -1;
-    if (len_sq != 0) //in case of 0 length line
+
+    /**
+     * Computes the shortest distance between a point and a finite line segment.
+     * This code has been adapted from a post on
+     * <a href="https://stackoverflow.com/a/6853926">StackOverflow</a>.
+     * @param p The point
+     * @param s The segment
+     * @return The shortest distance
+     */
+    protected static double distanceToSegment(Point p, Segment s)
     {
-        param = dot / len_sq;
+        double x = p.getX();
+        double y = p.getY();
+        double x1 = s.getaPoint().getX();
+        double y1 = s.getaPoint().getY();
+        double x2 = s.getbPoint().getX();
+        double y2 = s.getbPoint().getY();
+        double A = x - x1;
+        double B = y - y1;
+        double C = x2 - x1;
+        double D = y2 - y1;
+        double dot = A * C + B * D;
+        double len_sq = C * C + D * D;
+        double param = -1;
+        if (len_sq != 0) //in case of 0 length line
+        {
+            param = dot / len_sq;
+        }
+        double xx, yy;
+        if (param < 0)
+        {
+            xx = x1;
+            yy = y1;
+        }
+        else if (param > 1)
+        {
+            xx = x2;
+            yy = y2;
+        }
+        else
+        {
+            xx = x1 + param * C;
+            yy = y1 + param * D;
+        }
+        double dx = x - xx;
+        double dy = y - yy;
+        return Math.sqrt(dx * dx + dy * dy);
     }
-    double xx, yy;
-    if (param < 0) 
-    {
-      xx = x1;
-      yy = y1;
-    }
-    else if (param > 1) 
-    {
-      xx = x2;
-      yy = y2;
-    }
-    else 
-    {
-      xx = x1 + param * C;
-      yy = y1 + param * D;
-    }
-    double dx = x - xx;
-    double dy = y - yy;
-    return Math.sqrt(dx * dx + dy * dy);
-  }
 }
+
