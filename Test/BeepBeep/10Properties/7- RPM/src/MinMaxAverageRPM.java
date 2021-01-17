15fec3e5a3fed6f0b3c387e03d32f5ca53bf68e6
marc <marc-antoine.plourde1@uqac.ca>
2021-01-17 15:36:28 -0500

    Update MinMaxAverageRPM.java

--- Test/BeepBeep/10Properties/7- RPM/src/MinMaxAverageRPM.java
+++ Test/BeepBeep/10Properties/7- RPM/src/MinMaxAverageRPM.java
@@ -1,9 +1,6 @@
 import ca.uqac.lif.cep.Connector;
 import ca.uqac.lif.cep.Pullable;
-import ca.uqac.lif.cep.functions.ApplyFunction;
-import ca.uqac.lif.cep.functions.Cumulate;
-import ca.uqac.lif.cep.functions.CumulativeFunction;
-import ca.uqac.lif.cep.functions.FunctionTree;
+import ca.uqac.lif.cep.functions.*;
 import ca.uqac.lif.cep.io.ReadLines;
 import ca.uqac.lif.cep.io.WriteToFile;
 import ca.uqac.lif.cep.json.JPathFunction;
@@ -17,7 +14,6 @@ import ca.uqac.lif.cep.tmf.KeepLast;
 import ca.uqac.lif.cep.tmf.Pump;
 import ca.uqac.lif.cep.tmf.QueueSource;
 import ca.uqac.lif.cep.util.Numbers;
-import ca.uqac.lif.json.JsonMap;
 import ca.uqac.lif.mtnp.plot.gnuplot.Scatterplot;
 
 import java.io.FileWriter;
@@ -27,6 +23,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
+
+import static ca.uqac.lif.cep.Connector.connect;
 
 
 public class MinMaxAverageRPM {
@@ -44,7 +42,7 @@ public class MinMaxAverageRPM {
 
 
         QueueSource count=new QueueSource();
-        QueueSource tableXAxis=new QueueSource();
+        QueueSource tableXAxis=new QueueSource().loop(false);
 
         UpdateTable rpmTable=new UpdateTableStream("time(second)","rpm");
         KeepLast kl=new KeepLast();
@@ -56,6 +54,9 @@ public class MinMaxAverageRPM {
 
 
         ApplyFunction parseData=new ApplyFunction(ParseJson.instance);
+        Fork dataFork=new Fork(2);
+        connect(parseData,dataFork);
+
         ApplyFunction jpfRPMData=new ApplyFunction(new FunctionTree( NumberValue.instance,(new JPathFunction("data.engine.rpm"))));
         Fork rpmFork=new Fork(4);
         Cumulate rpmMin= new Cumulate(new CumulativeFunction<Number>(Numbers.minimum));
@@ -66,31 +67,36 @@ public class MinMaxAverageRPM {
         Pullable maxPull=rpmMax.getPullableOutput();
         Pullable averagePull=division.getPullableOutput();
 
-        Connector.connect(read,parseData);
-        Connector.connect(parseData,jpfRPMData);
-        Connector.connect(jpfRPMData,rpmFork);
-        Connector.connect(rpmFork,1,rpmMin,0);
-        Connector.connect(rpmFork,2,rpmMax,0);
-        Connector.connect(rpmFork,3,sum,0);
-        Connector.connect(sum,0,division,0);
-        Connector.connect(count,0,division,1);
+        connect(read,parseData);
+        connect(dataFork,0,jpfRPMData,0);
+        connect(jpfRPMData,rpmFork);
+        connect(rpmFork,1,rpmMin,0);
+        connect(rpmFork,2,rpmMax,0);
+        connect(rpmFork,3,sum,0);
+        connect(sum,0,division,0);
+        connect(count,0,division,1);
+
+        //Time
+        ApplyFunction timeData= new ApplyFunction(new JPathFunction("time"));
+        ApplyFunction time= new ApplyFunction(new FunctionTree(new getTime(), StreamVariable.X));
+        connect(dataFork,1,timeData,0);
+        connect(timeData,time);
+        Pullable timeP=time.getPullableOutput();
+
+        connect(tableXAxis,0,rpmTable,0);
+        connect(rpmFork,0,rpmTable,1);
+        connect(rpmTable,kl);
+        connect(kl,rpmDraw);
+        connect(kl,rpmWrite);
+        connect(rpmpump,rpmWrite);
+        connect(rpmDraw,rpmpump);
 
 
-        Connector.connect(tableXAxis,0,rpmTable,0);
-        Connector.connect(rpmFork,0,rpmTable,1);
-        Connector.connect(rpmTable,kl);
-        Connector.connect(kl,rpmDraw);
-        Connector.connect(kl,rpmWrite);
-        Connector.connect(rpmpump,rpmWrite);
-        Connector.connect(rpmDraw,rpmpump);
 
 
 
 
-
-        float time=0;
         int i=1;
-        Object[] out= new Object[1];
         float min=0;
         float max=0;
         float average=0;
@@ -99,9 +105,8 @@ public class MinMaxAverageRPM {
         while(readp.hasNext()){
 
             count.addEvent(i);
-            tableXAxis.addEvent(time);
+            tableXAxis.addEvent(timeP.pull());
             i++;
-            time+=0.10;
             min=(Float) minPull.pull();
             max=(Float) maxPull.pull();
             average=(Float) averagePull.pull();
